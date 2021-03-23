2ab42d7e3f1b9c224cfcd359ee6a0eab68d6aaa3
Heather <heathermhargreaves@users.noreply.github.com>
2021-03-22 21:52:55 -0400

    Update vcard-generator.js

--- twilio/functions/vcard-generator.js
+++ twilio/functions/vcard-generator.js
@@ -1,18 +1,17 @@
 const vCardsJS = require('vcards-js');
 exports.handler = async(context, event, callback) => {
   try {
-    // const {firstName, middleName, lastName, organization, cellPhone} = event;
     const vCard = vCardsJS();
-    vCard.firstName = "Talbots";
-    vCard.lastName = "customer service";
-    vCard.organization = "Talbots";
+    vCard.firstName = "New York Flagship";
+    vCard.lastName = "Store";
+    vCard.organization = "Your Favorite Store";
     vCard.cellPhone = event.store_number;
     
     console.log(vCard)
     
     const response = new Twilio.Response(); 
-    response.appendHeader('Content-Disposition', 'inline; filename="enesser.vcf"');
-    response.appendHeader('Content-Type', 'text/vcard; name="enesser.vcf"');
+    response.appendHeader('Content-Disposition', 'inline; filename="store.vcf"');
+    response.appendHeader('Content-Type', 'text/vcard; name="store.vcf"');
     response.setBody(vCard.getFormattedString());
     return callback(null, response);
   } catch(e) {
